3adbe1ed61171a0055aeea9722455a4090e64802
zcallan <kaln.delbridge@hotmail.com>
2023-03-14 18:18:23 +1100

    updated api call to use auth header, update svelte config

--- web/src/lib/utils/apiCall.ts
+++ web/src/lib/utils/apiCall.ts
@@ -1,18 +1,35 @@
 import { PUBLIC_API_URL } from '$env/static/public';
+import cookie from 'cookie';
+
+const deleteCookie = (name: string) => {
+  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
 
 async function apiCall<T>(url: string, options?: RequestInit): Promise<T> {
+  const cookies = cookie.parse(document.cookie);
+
+  const headers: any = {
+    'Content-Type': 'application/json',
+    ...options?.headers,
+  };
+
+  if (cookies?.hanko) {
+    headers.Authorization = `Bearer ${cookies.hanko}`;
+  }
+
   const res = await fetch(`${PUBLIC_API_URL}${url}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      ...options?.headers,
-    },
-    credentials: 'include',
+    headers,
     ...options,
   });
 
   const data = await res.json();
 
   if (res.status >= 400) {
+    if (res.status === 401) {
+      // deleteCookie('hanko');
+      // window.location.href = '/login';
+    }
+
     throw new Error(data?.message || 'Something went wrong');
   }
 
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,6 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
+// import adapter from '@sveltejs/adapter-cloudflare-workers';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
