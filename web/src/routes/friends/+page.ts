21d45015610e88fa162fdd485425366d6e8812ed
zcallan <kaln.delbridge@hotmail.com>
2023-03-14 18:48:14 +1100

    (web) removed server prefetching

--- web/src/routes/friends/+page.ts
+++ web/src/routes/friends/+page.ts
@@ -2,10 +2,9 @@ import { getFriends } from '$lib/api/friends';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ parent }) => {
-  const { queryClient } = await parent();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['friends'],
-    queryFn: getFriends,
-  });
+  // const { queryClient } = await parent();
+  // await queryClient.prefetchQuery({
+  //   queryKey: ['friends'],
+  //   queryFn: getFriends,
+  // });
 };
--- web/src/routes/posts/+page.ts
+++ web/src/routes/posts/+page.ts
@@ -3,12 +3,10 @@ import type { PostsFindAllQuery } from '$lib/types/generated';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ parent }) => {
-  const { queryClient } = await parent();
-
-  const options: PostsFindAllQuery = { page: 1, per_page: 10 };
-
-  await queryClient.prefetchQuery({
-    queryKey: ['posts', options],
-    queryFn: () => getPosts(options),
-  });
+  // const { queryClient } = await parent();
+  // const options: PostsFindAllQuery = { page: 1, per_page: 10 };
+  // await queryClient.prefetchQuery({
+  //   queryKey: ['posts', options],
+  //   queryFn: () => getPosts(options),
+  // });
 };
